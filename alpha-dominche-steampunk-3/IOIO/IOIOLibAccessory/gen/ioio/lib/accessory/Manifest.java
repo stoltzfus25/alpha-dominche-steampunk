86f15ced1b5177f0d949d279384aaff6d35aa582
Samuel Stoltzfus <samuel@superiorplastic.net>
2024-03-19 19:37:11 -0400

    Remove ignored files

--- alpha-dominche-steampunk-3/IOIO/IOIOLibAccessory/gen/ioio/lib/accessory/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package ioio.lib.accessory;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
